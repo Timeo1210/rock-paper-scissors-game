4742ad0a0919456b3af6040aeac42946f7cd5b6a
Timeo1210 <timeo.boulhol@gmail.com>
2021-02-23 18:46:58 +0100

    remove draggable image and user selection Prepared Item to handleAction

--- components/Item/Item.tsx
+++ components/Item/Item.tsx
@@ -1,21 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import styles from "./Item.module.scss";
 
 interface ItemProps {
   itemType: ItemTypes;
+  handleAction?: (itemType: ItemTypes) => void;
 }
 
-const Item: React.FC<ItemProps> = ({ itemType }) => {
+const Item: React.FC<ItemProps> = ({ itemType, handleAction }) => {
+  const containerRef = useRef(null);
+
   const [pressed, setPressed] = useState(false);
 
   const imgSrc = `/images/icon-${itemType}.svg`;
   const background = `var(--gradient-${itemType})`;
 
+  useEffect(() => {
+    const handlePointerUp = (e) => {
+      e.preventDefault();
+      setPressed(false);
+    };
+    const handlePointerDown = (e) => {
+      e.preventDefault();
+      if (containerRef.current.contains(e.target)) {
+        setPressed(true);
+      }
+    };
+
+    window.addEventListener("mouseup", handlePointerUp);
+    window.addEventListener("mousedown", handlePointerDown);
+
+    return () => {
+      window.removeEventListener("mouseup", handlePointerUp);
+      window.removeEventListener("mousedown", handlePointerDown);
+    };
+  }, []);
+
   return (
     <div
-      onPointerDown={() => setPressed(true)}
-      onPointerUp={() => setPressed(false)}
+      ref={containerRef}
+      onClick={() => handleAction(itemType)}
       className={styles.container}
     >
       <div
@@ -28,7 +52,7 @@ const Item: React.FC<ItemProps> = ({ itemType }) => {
       ></div>
       <div className={styles.imageWrapper}>
         <div className={styles.square}>
-          <img className={styles.image} src={imgSrc} />
+          <img draggable="false" className={styles.image} src={imgSrc} />
         </div>
       </div>
     </div>
