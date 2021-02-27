39fb879055c894bfb0ce3956b6d27421ca2264fe
Timeo1210 <timeo.boulhol@gmail.com>
2021-02-27 22:58:47 +0100

    add WhoWin type

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 type ItemTypes = "rock" | "paper" | "scissors";
+type WhoWin = "user" | "house" | "none";
 interface TransitionProps {
   inProps: boolean;
   onExited?: () => void;
