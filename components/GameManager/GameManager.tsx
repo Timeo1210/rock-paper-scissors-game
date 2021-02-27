5ef8dcb3cbf71986114d8c95fe7f880e6edab896
Timeo1210 <timeo.boulhol@gmail.com>
2021-02-27 18:37:49 +0100

    handle generating of houseItem

--- components/GameManager/GameManager.tsx
+++ components/GameManager/GameManager.tsx
@@ -1,33 +1,42 @@
-import React, { useReducer, useState } from "react";
-
 import ItemPicker from "@/components/ItemPicker";
 import ShowItems from "@/components/ShowItems";
-
 import ItemPickerTransition from "@/Transitions/ItemPickerTransition";
 import ShowItemsTransition from "@/Transitions/ShowItemsTransition";
+import React, { useEffect, useState } from "react";
 
 type ShowComponentValues = "ItemPicker" | "ShowItems";
 
-const GameManager: React.FC<{}> = ({ children }) => {
+const AvailableItems: ItemTypes[] = ["paper", "rock", "scissors"];
+const generateRandomItem = () => {
+  const index = Math.floor(Math.random() * AvailableItems.length);
+  return AvailableItems[index];
+};
+
+const GameManager: React.FC<{}> = () => {
   const [showComponent, setShowComponent] = useState<ShowComponentValues>(
     "ItemPicker"
   );
   const [userItem, setUserItem] = useState<ItemTypes | null>(null);
+  const [houseItem, setHouseItem] = useState<ItemTypes | null>(null);
+
+  const handleItemPicked = () => {
+    setShowComponent("ShowItems");
+    setTimeout(() => {
+      setHouseItem(generateRandomItem());
+    }, 1000);
+  };
 
   switch (showComponent) {
     case "ShowItems":
       return (
         <ShowItemsTransition inProps={!!userItem}>
-          <ShowItems userItem={userItem} />
+          <ShowItems userItem={userItem} houseItem={houseItem} />
         </ShowItemsTransition>
       );
 
     default:
       return (
-        <ItemPickerTransition
-          inProps={!userItem}
-          onExited={() => setShowComponent("ShowItems")}
-        >
+        <ItemPickerTransition inProps={!userItem} onExited={handleItemPicked}>
           <ItemPicker setUserItem={setUserItem} />
         </ItemPickerTransition>
       );
--- components/ShowItems/ShowItems.tsx
+++ components/ShowItems/ShowItems.tsx
@@ -6,17 +6,18 @@ import Item from "@/components/Item";
 
 interface ShowItemsProps {
   userItem: ItemTypes;
+  houseItem: ItemTypes;
 }
 
-const ShowItems: React.FC<ShowItemsProps> = ({ userItem }) => {
+const ShowItems: React.FC<ShowItemsProps> = ({ userItem, houseItem }) => {
   return (
     <div className={styles.container}>
       <div className={styles.item}>
-        <Item itemType={userItem} />
+        <Item itemType={userItem} win={true} />
         <span>YOU PICKED</span>
       </div>
       <div className={styles.item}>
-        <Item itemType={null} />
+        <Item itemType={houseItem} />
         <span>THE HOUSE PICKED</span>
       </div>
     </div>
