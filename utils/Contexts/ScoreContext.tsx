6fa2acfc3b284023b1c0d35b9371e395ae972166
Timeo1210 <timeo.boulhol@gmail.com>
2021-02-28 03:25:24 +0100

    fix ScoreContext

--- utils/Contexts/ScoreContext.tsx
+++ utils/Contexts/ScoreContext.tsx
@@ -2,15 +2,26 @@
 /* eslint-disable react/no-unused-state */
 import React from "react";
 
-export const ScoreContext = React.createContext(null);
+interface ScoreType {
+  score: number;
+  setScore: (newScore: number) => void;
+}
 
-export class ScoreProvider extends React.Component {
+export const ScoreContext = React.createContext({
+  score: 0,
+} as ScoreType);
+
+export class ScoreProvider extends React.Component<{}, ScoreType> {
   constructor(props) {
     super(props);
 
     this.state = {
       score: 0,
-    };
+      setScore: (newScore) =>
+        this.setState({
+          score: newScore,
+        }),
+    } as ScoreType;
   }
 
   render() {
