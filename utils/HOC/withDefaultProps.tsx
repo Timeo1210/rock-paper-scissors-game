191e6bc33986e39fc2e0e00de7d3c822d2feb803
Timeo1210 <timeo.boulhol@gmail.com>
2021-02-28 02:24:02 +0100

    add withDefaultProps HOC

---
+++ utils/HOC/withDefaultProps.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function withDefaultProps<T>(
+  WrappedComponent: React.ComponentType<T>,
+  ...defaultProps: any
+) {
+  return (props: T) => {
+    <WrappedComponent {...defaultProps} {...props} />;
+  };
+}
+
+export default withDefaultProps;
